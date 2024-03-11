e9355a50cb09d65db877d4d809058f982dd8562c
trantuvan <trantuvan.kan@gmail.com>
2024-03-11 20:46:44 +0700

    convert text to html

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -3,6 +3,15 @@ from ddt import ddt, data, unpack
 from typing import Tuple
 
 from textnode import TextNode
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_image,
+    text_type_link,
+)
 
 @ddt
 class TestTextNode(unittest.TestCase):
@@ -40,5 +49,32 @@ class TestTextNode(unittest.TestCase):
         test = TextNode(text, text_type, url)
         self.assertEqual(repr(test), f"TextNode({text}, {text_type}, {url})", "Representation tests")
 
+    def test_eq(self):
+        node = TextNode("This is a text node", text_type_text)
+        node2 = TextNode("This is a text node", text_type_text)
+        self.assertEqual(node, node2)
+
+    def test_eq_false(self):
+        node = TextNode("This is a text node", text_type_text)
+        node2 = TextNode("This is a text node", text_type_bold)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", text_type_text)
+        node2 = TextNode("This is a text node2", text_type_text)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+
 if __name__ == "__main__":
     unittest.main()
+
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,12 @@
+from leafnode import LeafNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
 class TextNode:
     def __init__(self, text:str, text_type:str, url:str=None) -> None:
         self.text = text
@@ -9,3 +18,19 @@ class TextNode:
     
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+def text_node_to_html_node(text_node:type["TextNode"])->str:
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text).to_html()
+    if text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text).to_html()
+    if text_node.text_type == text_type_italic:
+        return LeafNode("i", text_node.text).to_html()
+    if text_node.text_type == text_type_code:
+        return LeafNode("code", text_node.text).to_html()
+    if text_node.text_type == text_type_link:
+        return LeafNode("a", text_node.text, {"href":"www.google.com"}).to_html()
+    if text_node.text_type == text_type_image:
+        return LeafNode("img", text_node.text, {"src":"www.google.com", "alt":"google imgs"}).to_html()
+
+    raise Exception("cannot convert to html text of type:", text_node.text_type)
