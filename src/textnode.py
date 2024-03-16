ac687931f03258ace8e25af94b9f13039bc25608
trantuvan <trantuvan.kan@gmail.com>
2024-03-16 16:02:22 +0700

    delimiters

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from leafnode import LeafNode
+from typing import List
 
 text_type_text = "text"
 text_type_bold = "bold"
@@ -6,6 +7,10 @@ text_type_italic = "italic"
 text_type_code = "code"
 text_type_link = "link"
 text_type_image = "image"
+
+delimiter_bold = "**"
+delimiter_italic = "*"
+delimiter_code = "`"
 
 class TextNode:
     def __init__(self, text:str, text_type:str, url:str=None) -> None:
@@ -34,3 +39,33 @@ def text_node_to_html_node(text_node:type["TextNode"])->str:
         return LeafNode("img", text_node.text, {"src":"www.google.com", "alt":"google imgs"}).to_html()
 
     raise Exception("cannot convert to html text of type:", text_node.text_type)
+
+def split_nodes_delimiter(old_nodes:List[TextNode], delimiter:str, text_type:str)-> List[TextNode]:
+    new_nodes = []
+    valid_text_types = [text_type_text,
+                        text_type_bold,
+                        text_type_italic,
+                        text_type_code,
+                        text_type_link,
+                        text_type_image]
+
+    for node in old_nodes:
+        if isinstance(node, TextNode) == False:
+            new_nodes.append(node)
+        if node.text_type not in valid_text_types:
+            continue
+        str_array = node.text.split(delimiter)
+
+        if str_array.__len__() % 2 == 0:
+            raise Exception("that's invalid Markdown syntax.")
+
+        for i in range(len(str_array)):
+            textNode = None
+            if i % 2 == 0:
+                textNode = TextNode(str_array[i], text_type_text)
+            else:
+                textNode = TextNode(str_array[i], text_type)
+            new_nodes.append(textNode)
+
+    return new_nodes
+
